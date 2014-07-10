f503c4bd5a03a0a1c171eb23844ed45deeac1b98
ahb <andreas.bihlmaier@gmx.de>
2014-07-10 15:39:55 +0200

    use planning_scene_monitor ... functionality nearly already there now

--- src/test.cpp
+++ src/test.cpp
@@ -2,53 +2,43 @@
 
 #include <moveit/robot_model_loader/robot_model_loader.h>
 #include <moveit/planning_scene/planning_scene.h>
+#include <moveit/planning_scene_monitor/planning_scene_monitor.h>
 
 
 
 
 int main(int argc, char **argv)
 {
-  // https://github.com/ros-planning/moveit_pr2/blob/hydro-devel/pr2_moveit_tutorials/planning/src/planning_scene_tutorial.cpp
   ros::init (argc, argv, "safe_cartesian_moveit_test");
   ros::AsyncSpinner spinner(1);
   spinner.start();
 
-  robot_model_loader::RobotModelLoader robot_model_loader("robot_description");
-  robot_model::RobotModelPtr kinematic_model = robot_model_loader.getModel();
-  planning_scene::PlanningScene planning_scene(kinematic_model);
+  planning_scene_monitor::PlanningSceneMonitorPtr planning_scene_monitor(new planning_scene_monitor::PlanningSceneMonitor("robot_description"));
+  planning_scene_monitor->requestPlanningSceneState();
+
+  planning_scene_monitor::LockedPlanningSceneRO locked_planning_scene(planning_scene_monitor);
+  //robot_model::RobotModelConstPtr robot_model = locked_planning_scene->getRobotModel();
+
+  const robot_state::RobotState& current_state = locked_planning_scene->getCurrentState();
+  robot_state::RobotState target_state(current_state);
+  // TODO target_state.setJointPositions(...)
+  target_state.update();
+
 
   collision_detection::CollisionRequest collision_request;
   collision_detection::CollisionResult collision_result;
-
-
-  robot_state::RobotState& current_state = planning_scene.getCurrentStateNonConst();
-  current_state.setToRandomPositions();
-  /*
-  collision_result.clear();
-  planning_scene.checkSelfCollision(collision_request, collision_result);
-  ROS_INFO_STREAM("Test 2: Current state is " << (collision_result.collision ? "in" : "not in") << " self collision");
-  */
-
-
-  collision_result.clear();
-  //collision_request.group_name = "lwr2";
+  // TODO collision_request.group_name = "lwr2";
   collision_request.contacts = true;
   collision_request.max_contacts = 1000;
-  planning_scene.checkCollision(collision_request, collision_result, current_state);
-  ROS_INFO_STREAM("Test 6: Current state is " << (collision_result.collision ? "in" : "not in") << " collision");
+  collision_result.clear();
+  locked_planning_scene->checkCollision(collision_request, collision_result, target_state);
+  ROS_INFO_STREAM("Target state is " << (collision_result.collision ? "in" : "not in") << " collision");
   collision_detection::CollisionResult::ContactMap::const_iterator it;
   for(it = collision_result.contacts.begin();
       it != collision_result.contacts.end();
       ++it) {
     ROS_INFO("Contact between: %s and %s", it->first.first.c_str(), it->first.second.c_str());
   }
-
-
-  /* TODO
-   * PlanningScene -> planning_scene_monitor::PlanningSceneMonitor; robot_model_loader -> getRobotModel()
-   * current_state.setJointPositions()
-   * planning_scene.checkCollision()
-   */
 
 
   ros::shutdown();
