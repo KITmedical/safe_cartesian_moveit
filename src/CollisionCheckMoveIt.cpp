2f79102f274a8b8fd51e02a11f6ae06326676a78
ahb <andreas.bihlmaier@gmx.de>
2014-07-22 14:02:00 +0200

    self collision checking now working

--- src/CollisionCheckMoveIt.cpp
+++ src/CollisionCheckMoveIt.cpp
@@ -6,15 +6,23 @@
 #include <moveit/robot_model_loader/robot_model_loader.h>
 
 // custom includes
+#include <ahbstring.h>
 
 
 /*---------------------------------- public: -----------------------------{{{-*/
+// TODO robot_description vs composite-robot_description
+// remap as in joint_topic_merger.py
+// use a map, fill it ONCE in some init function
 CollisionCheckMoveIt::CollisionCheckMoveIt()
   :m_planning_scene_monitor(new planning_scene_monitor::PlanningSceneMonitor("robot_description")),
    m_locked_planning_scene(m_planning_scene_monitor)
 {
   ros::AsyncSpinner spinner(1);
   spinner.start();
+
+  if (ros::param::get("single_to_composite_joints", m_joint_names_map)) {
+    ROS_INFO_STREAM("Using joint mappings: " << ahb::string::toString(m_joint_names_map));
+  }
 
   m_planning_scene_monitor->requestPlanningSceneState();
 }
@@ -27,13 +35,18 @@ CollisionCheckMoveIt::getCollisionResult(const sensor_msgs::JointState& targetJo
 
   robot_state::RobotState target_state(current_state);
   for (size_t jointIdx = 0; jointIdx < targetJointsState.name.size(); jointIdx++) {
-    target_state.setJointPositions(targetJointsState.name[jointIdx], &targetJointsState.position[jointIdx]);
+    std::string joint_name;
+    if (m_joint_names_map.size() == 0) {
+      joint_name = targetJointsState.name[jointIdx];
+    } else {
+      joint_name = m_joint_names_map[targetJointsState.name[jointIdx]];
+    }
+    target_state.setJointPositions(joint_name, &targetJointsState.position[jointIdx]);
   }
   target_state.update();
 
   collision_detection::CollisionRequest collision_request;
   collision_detection::CollisionResult collision_result;
-  // TODO collision_request.group_name = "lwr2";
   collision_request.contacts = contacts;
   collision_request.max_contacts = 100;
   collision_result.clear();
